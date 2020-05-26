46abae70d8846d8ba562196c26ef38625cc3cd24
DrewWham <fcwham@gmail.com>
2020-05-26 14:06:31 -0400

    add utility functions

---
+++ Lectures/1.The_R_environment_and_Base_R/Utility_functions.R
@@ -0,0 +1,55 @@
+
+paste("Hello","world",sep=" ")
+
+words<-c("Hello","world")
+
+paste(words,sep=" ")
+
+paste(words,collapse = " ")
+
+hello<-"Hello"
+
+names_vec<-c("Drew","Paul","Sandy","Erica")
+
+paste(hello,names_vec,sep=" ")
+
+paste0(hello,names_vec)
+
+paste0(hello," ",names_vec)
+
+num_series<-1:10
+
+num_series2<-seq(0,100,10)
+
+hello_10<-rep("Hello",10)
+
+rnorm(10,1,1)
+
+sample(num_series,5)
+
+rand_samp<-sample(num_series,15,replace = T)
+
+max(rand_samp)
+min(rand_samp)
+
+lenght(rand_samp)
+
+unique(rand_samp)
+
+sum(2,5)
+
+2+5
+
+sum(2,5,7)
+
+a<-c(2,1,0)
+
+b<-c(4,3,8)
+
+sum(a,b)
+
+a+b
+
+
+
+

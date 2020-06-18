3836b42b07f52f8f38303537da26804ee2a98a69
DrewWham <fcwham@gmail.com>
2020-06-18 09:21:29 -0400

    update keys and merging

--- Lectures/5.Merging/Keys and Merging.R
+++ Lectures/5.Merging/Keys and Merging.R
@@ -5,47 +5,23 @@ library(data.table)
 
 
 #This reads in the flight data and stores it as an object called 'DT'
-DT<-fread("./Lectures/Data/Flights/2008.csv")
+DT<-fread("../Data/Flights/2008.csv")
 #This reads in the data about airports and stores it as an object called 'AP'
-AP<-fread("./Lectures/Data/Flights/airports.csv")
+AP<-fread("../Data/Flights/airports.csv")
 
 
-merge_DT<-merge(DT,AP,by.x = "Origin",by.y="iata_code")
-
-
-system.time(DT[Origin == "SFO"])
-system.time(merge(DT,AP,by.x = "Origin",by.y="iata_code"))
-
-
-setnames(AP,"iata_code","Origin")
-
-setkey(DT,Origin)
-setkey(AP,iata_code)
-
-system.time(merge(DT,AP))
 
 
 #calculate some aggregate stats 
-Avg_DepDelay<-dcast(DT,Origin~.,mean,na.rm=T,value.var=c("DepDelay"))
-Avg_Dist<-dcast(DT,Origin~.,mean,na.rm=T,value.var=c("Distance"))
-Avg_TaxTime<-dcast(DT,Origin~.,mean,na.rm=T,value.var=c("TaxiOut"))
+Avg_DepDelay<-DT[,.(Avg_DepDelay=mean(DepDelay,na.rm=T)),by=Origin]
+Avg_Dist<-DT[,.(Avg_Dist=mean(Distance,na.rm=T)),by=Origin]
+Avg_TaxTime<-DT[,.(Avg_TaxTime=mean(TaxiOut,na.rm=T)),by=Origin]
 
-#rename the new values
-setnames(Avg_DepDelay,".","Avg_DepDelay")
-setnames(Avg_Dist,".","Avg_Dist")
-setnames(Avg_TaxTime,".","Avg_TaxTime")
+
+# before showing prefered method:
+merge_DT<-merge(DT,AP,by.x = "Origin",by.y="iata_code")
+
 setnames(AP,"iata_code","Origin")
-
-#write out .csv to upload to canvas
-fwrite(Avg_DepDelay[1:100,],"sub_Avg_DepDelay.csv")
-fwrite(Avg_Dist[1:100,],"sub_Avg_Dist.csv")
-fwrite(Avg_TaxTime[1:100,],"sub_Avg_TaxTime.csv")
-
-
-#make sure the objects are data.tables
-Avg_DepDelay<-data.table(Avg_DepDelay)
-Avg_Dist<-data.table(Avg_Dist)
-Avg_TaxTime<-data.table(Avg_TaxTime)
 
 #merging with keys
 #first set some keys
@@ -60,4 +36,6 @@ Airport_stats<-merge(Avg_DepDelay,AP,all.x=T)
 Airport_stats<-merge(Airport_stats,Avg_Dist,all.x=T)
 Airport_stats<-merge(Airport_stats,Avg_TaxTime,all.x=T)
 
-fwrite(Airport_stats,"./Homework/Airport_stats.csv")
+Airport_stats<-Airport_stats[,.(Origin,name,latitude_deg,longitude_deg,Avg_DepDelay,Avg_Dist,Avg_TaxTime)]
+
+fwrite(Airport_stats,"../Data/Flights/Airport_stats.csv")

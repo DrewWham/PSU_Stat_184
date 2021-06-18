faefa92d69ee2cd26f62935ece315b3bc5d2db74
DrewWham <fcwham@gmail.com>
2021-06-18 09:19:00 -0400

    remove set.seed from submit code

--- code_scripts.R
+++ code_scripts.R
@@ -1,9 +1,8 @@
 
 
 submitCode5<-function(user_id,x){
-  set.seed(4)
   size<-5
-  code<-sample(c(1,0),size,replace=T)
+  code<-c(1,1,0,1,0)
   sub_code<-as.numeric(strsplit(x,"")[[1]])
   percent<-sum(code==sub_code)/size
   out<-percent
@@ -12,9 +11,9 @@ submitCode5<-function(user_id,x){
 }
 
 submitCode256<-function(user_id,x){
-  set.seed(4)
   size<-256
-  code<-sample(c(1,0),size,replace=T)
+  code_string<-"0111101010100000010111110011001011101001011001010000101000001111000000000010011001110001110110100010100110000011011010001101101101111111000000111111110011111011110100111011001011011000011100110011110010110100001000001010011000100001000111101101110110101000"
+  code<-as.numeric(strsplit(code_string,"")[[1]])
   sub_code<-as.numeric(strsplit(x,"")[[1]])
   percent<-sum(code==sub_code)/size
   out<-percent
